f89b38e047ce0906b9f9859390abb933cc00dc5d
John Kristan Torremocha <torremocha.johnkristan@gmailo.com>
2026-01-20 05:12:33 +0800

    style: static project dashboard w/ shadcn components

--- client/app/routes.ts
+++ client/app/routes.ts
@@ -1,3 +1,6 @@
-import { type RouteConfig, index } from "@react-router/dev/routes";
+import { type RouteConfig, index, route } from "@react-router/dev/routes";
 
-export default [index("routes/home.tsx")] satisfies RouteConfig;
+export default [
+  index("pages/auth/login.tsx"),
+  route("projects/dashboard", "./pages/projects-dashboard.tsx"),
+] satisfies RouteConfig;
