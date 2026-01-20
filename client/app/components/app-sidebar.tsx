43cadbfb12a4602c3f3a72e803c80ae660c2afc0
John Kristan Torremocha <138425887+johnkristanf@users.noreply.github.com>
2026-01-20 12:38:29 +0800

    chore: removed unused variables

--- client/app/components/app-sidebar.tsx
+++ client/app/components/app-sidebar.tsx
@@ -19,7 +19,6 @@ import { Link } from "react-router"
 
 // This is sample data.
 const data = {
-  versions: ["1.0.1", "1.1.0-alpha", "2.0.0-beta1"],
   navMain: [
     {
       title: "Getting Started",
